295ecea50e53eecb4663129cd6a890fbd9f89ae1
Sanidhya Singh <singhsanidhya15@gmail.com>
2026-09-18 09:30:28 +0530

    Add mobile menu toggle and form submission function

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.getElementById('mobileMenu').classList.toggle('open');
+}
+
+function closeMobile() {
+  document.getElementById('mobileMenu').classList.remove('open');
+}
+
+function handleSubmit() {
+  alert('Thank you for your enquiry! We will contact you at +91 92140 73500 shortly.');
+}
